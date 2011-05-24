b6f8aefa1a99e1681d220c54d78c74a46c7d4776
grifgrif <devnull@localhost>
2011-05-24 16:35:45 +0000

    First try at facilitating vectorization of array expressions by chunking them up in TinyVector expressions. Does not have correct alignment or padding, proof-of-concept only.

--- blitz/tinyvec.h
+++ blitz/tinyvec.h
@@ -433,6 +433,19 @@ template<typename T>
 class TinyVector<T,0> {
 };
 
+// Helper class that defines the width of the simd instructions for a
+// given type
+template<typename T> class simdTypes {
+public:
+  // simd width of type in bytes
+  static const int byteWidth = 
+    BZ_SIMD_WIDTH>sizeof(T) ? BZ_SIMD_WIDTH : sizeof(T);
+  // simd width of types in number of elements
+  static const int vecWidth=
+    BZ_SIMD_WIDTH>sizeof(T) ? BZ_SIMD_WIDTH/sizeof(T) : 1;
+  typedef TinyVector<T, vecWidth> vecType;
+};
+
 BZ_NAMESPACE_END
 
 #include <blitz/tinyveciter.h>  // Iterators
